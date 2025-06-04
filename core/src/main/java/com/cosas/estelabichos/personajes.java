7dbb18373e42d4ac03d8723cef6ba97f3251fdd3
yo_fp <papas@correo.com>
2025-06-04 10:16:22 +0200

    palo merge

--- core/src/main/java/com/cosas/estelabichos/personajes.java
+++ core/src/main/java/com/cosas/estelabichos/personajes.java
@@ -7,18 +7,19 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class personajes {
 
-    int width,height;
-    float cordX,cordY;
+    int width, height;
+    float cordX, cordY;
     Sprite forma;
 
-    personajes(int width, int height, float cordX, float cordY, Texture piel){
-        this.width=width;
-        this.height=height;
-        this.cordX=cordX;
-        this.cordY=cordY;
-        forma=new Sprite(piel);
+    personajes(int width, int height, float cordX, float cordY, Texture piel) {
+        this.width = width;
+        this.height = height;
+        this.cordX = cordX;
+        this.cordY = cordY;
+        forma = new Sprite(piel);
     }
 
-    public void movimiento(){}
+    public void movimiento() {
+    }
 
 }
