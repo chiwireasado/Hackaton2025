13df874c55ccd6378bdb2241316ceb495020536c
yo_fp <papas@correo.com>
2025-06-06 11:19:15 +0200

    no se sale de los bordes.

--- core/src/main/java/com/cosas/estelabichos/Main.java
+++ core/src/main/java/com/cosas/estelabichos/Main.java
@@ -11,7 +11,10 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.rmi.dgc.DGC;
 
@@ -20,18 +23,26 @@ public class Main extends Game {
 
 
     Array<Sprite> enemigos;
+    Array<Sprite> auras;
+
 
     SpriteBatch batch;
     Sprite prota,fondo,enemigo,aura;
     Rectangle hitBXprota,hitBXenemigo,hbxaura;
 
 
+    static float ranX;
+    static float ranY;
     Camera camara;
     static final int world_width= 600;
     static final int world_height= 440;
 
-    public void movimiento_enemigo(){
-        enemigo.translate(1,1);
+    public void movimiento_enemigo() {
+        float delta = Gdx.graphics.getDeltaTime();
+        enemigo.translate(ranX * delta, ranY * delta);
+        enemigo.setX(MathUtils.clamp(enemigo.getX(),0,world_width-hitBXenemigo.getWidth()));
+        enemigo.setY(MathUtils.clamp(enemigo.getY(),0,world_height-hitBXenemigo.getHeight()));
+
     }
 
     public void movimiento_personaje(){
@@ -39,18 +50,38 @@ public class Main extends Game {
         float delta= Gdx.graphics.getDeltaTime();
 
         if (Gdx.input.isKeyPressed(Input.Keys.W)){
-            prota.translateY(speed*delta);
+            prota.translateY(speed);
         }
         else if (Gdx.input.isKeyPressed(Input.Keys.S)){
-            prota.translateY(-speed*delta);
+            prota.translateY(-speed);
         }
         else if (Gdx.input.isKeyPressed(Input.Keys.A)){
-            prota.translateX(-speed*delta);
+            prota.translateX(-speed);
         }
         else if (Gdx.input.isKeyPressed(Input.Keys.D)){
-            prota.translateX(speed*delta);
+            prota.translateX(speed);
         }
+
+        prota.setX(MathUtils.clamp(prota.getX(),0,world_width));
+        prota.setY(MathUtils.clamp(prota.getY(),0,world_height));
+
+        if (prota.getX()>=world_width){
+            System.out.println("tocamos X");
+        }
+        if (prota.getY()>=world_height){
+            System.out.println("tocamos Y");
+        }
+
+        if (prota.getY()<=(-world_height)){
+            System.out.println("tocamos -Y");
+        }
+
+        if (prota.getX()<=(-world_width)){
+            System.out.println("tocamos -X");
+        }
+
     }
+
 
 
     @Override
@@ -60,7 +91,7 @@ public class Main extends Game {
         // multiples enemigos
         enemigos=new Array<>(5);
 
-
+        // cracion aura
         aura=new Sprite(new Texture(Gdx.files.internal("bicho.png")));
         hbxaura=new Rectangle();
 
@@ -69,6 +100,7 @@ public class Main extends Game {
         fondo.setPosition(20,20);
         fondo.setSize(world_width,world_height);
 
+        // cosas aun no usadas
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
         camara= new OrthographicCamera(30, 30 * (h / w));
@@ -82,18 +114,19 @@ public class Main extends Game {
         // todo sobre el enemigo
         enemigo=new Sprite(new Texture(Gdx.files.internal( "bicho.png")));
         enemigo.setSize(50,50);
-        float ranX=MathUtils.random(0,fondo.getWidth()-enemigo.getWidth());
-        float ranY=MathUtils.random(0,fondo.getHeight()-enemigo.getHeight());
+        ranX=MathUtils.random(0,fondo.getWidth()-enemigo.getWidth());
+        ranY=MathUtils.random(0,fondo.getHeight()-enemigo.getHeight());
         enemigo.setPosition(ranX,ranY);
 
 
+        //hitbox's iniciadas
         hitBXprota=new Rectangle();
         hitBXenemigo=new Rectangle();
 
 
-
         camara.update();
         batch=new SpriteBatch();
+
 
 
     }
@@ -104,14 +137,18 @@ public class Main extends Game {
         camara.update();
 
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        movimiento_personaje();
 
-        //hitbox's
+        // movimientos
+        movimiento_personaje();
+        movimiento_enemigo();
+
+
+        //hitbox's configuracion
         hitBXprota.set(prota.getX(), prota.getY(), prota.getWidth()/2, prota.getHeight()/2);
         hitBXenemigo.set(enemigo.getX(),enemigo.getY(),enemigo.getWidth()/2,enemigo.getHeight()/2);
 
 
-
+        // colision
         if (hitBXprota.overlaps(hitBXenemigo)) {
             System.out.println("choco");
             //prota.setPosition((prota.getX()-prota.getWidth()/2),(prota.getY()-prota.getHeight()/2));
@@ -119,12 +156,9 @@ public class Main extends Game {
         }
 
 
-
         if(Gdx.input.isKeyJustPressed(Input.Keys.P)){
             System.out.println("Prota X: "+(prota.getX()-prota.getWidth())+"---Y: "+(prota.getY()-prota.getHeight()+""));
         }
-
-        movimiento_enemigo();
 
 
 
@@ -134,9 +168,7 @@ public class Main extends Game {
         batch.begin();
         fondo.draw(batch);
         prota.draw(batch);
-
         enemigo.draw(batch);
-
         batch.end();
 
     }
@@ -144,6 +176,8 @@ public class Main extends Game {
     @Override
     public void dispose() {
         super.dispose();
+        batch.dispose();
+
     }
 
     @Override
